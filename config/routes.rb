6472edc952d8423b5dda7d6d787af2a4822a217a
WillyWG <willywg@gmail.com>
2019-06-13 13:08:15 -0500

    Add webpacker stack

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   end
   post "/graphql", to: "graphql#execute"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'home#index'
 end
